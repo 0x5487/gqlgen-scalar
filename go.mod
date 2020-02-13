ab8fa3e53085b6da623b7d0d3fb5e96aab0a6a2a
JasonLee <jawc@hotmail.com>
2020-02-13 15:51:01 +0800

    add Int64 type

---
+++ go.mod
@@ -0,0 +1,3 @@
+module scalar
+
+go 1.13
---
+++ scalar.go
@@ -0,0 +1,25 @@
+package scalar
+
+import (
+	"fmt"
+	"io"
+)
+
+type Int64 int64
+
+// UnmarshalGQL implements the graphql.Unmarshaler interface
+func (i *Int64) UnmarshalGQL(v interface{}) error {
+	val, ok := v.(int64)
+	if !ok {
+		return fmt.Errorf("value must be int64 type")
+	}
+
+	*i = Int64(val)
+
+	return nil
+}
+
+// MarshalGQL implements the graphql.Marshaler interface
+func (i Int64) MarshalGQL(w io.Writer) {
+	fmt.Fprintf(w, `"%d"`, i)
+}
